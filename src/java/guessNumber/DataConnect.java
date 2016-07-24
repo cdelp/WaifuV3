0ed8217a154307f92a5e316af4745d8e60cbf86b
Chad Delp <cdelp@hawaii.edu>
2016-07-24 10:00:27 -1000

    Added Cynthia's login files

---
+++ src/java/guessNumber/DataConnect.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package guessNumber;
+
+/**
+ * Connects to our db
+ * @author Masako
+ */
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DataConnect {
+
+    public static Connection getConnection() {
+        try {
+            /* Edit the username whatever here */
+            String dbURL = "jdbc:derby://localhost:1527/ics";
+            String user = "DBUSER";
+            String password = "ics";
+
+            Connection con = DriverManager.getConnection(dbURL, user, password);
+            return con;
+        } catch (Exception ex) {
+            System.out.println("Database.getConnection() Error -->"
+                    + ex.getMessage());
+            return null;
+        }
+    }
+
+    public static void close(Connection con) {
+        try {
+            con.close();
+        } catch (Exception ex) {
+        }
+    }
+}
---
+++ src/java/guessNumber/Login.java
@@ -0,0 +1,119 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+
+ Using http://www.journaldev.com/7252/jsf-authentication-login-logout-database-example 
+ And also https://platform.netbeans.org/tutorials/60/nbm-login.html for the md5 hash code
+
+A bunch of getters and setters (for now)
+When registration is implemented, we will need to change some things. 
+ */
+package guessNumber;
+
+import java.io.Serializable;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+import guessNumber.LoginDAO;
+import guessNumber.SessionUtils;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+@ManagedBean
+@SessionScoped
+public class Login implements Serializable {
+
+    //I honestly don't know what this is for. This is just copied for now until we need to modify this for md5 hashing, which will come later
+    private static final long serialVersionUID = 1094801825228386363L;
+
+    private String pwd;
+    private String msg;
+    private String user;
+
+    public String getPwd() {
+        return pwd;
+    }
+
+    public void setPwd(String pwd) {
+        this.pwd = pwd;
+
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    //validate login
+    public String validateUsernamePassword() {
+        boolean valid = LoginDAO.validate(user, pwd);
+        if (valid) {
+            HttpSession session = SessionUtils.getSession();
+            session.setAttribute("username", user);
+            return "admin";
+        } else {
+            FacesContext.getCurrentInstance().addMessage(
+                    null,
+                    new FacesMessage(FacesMessage.SEVERITY_WARN,
+                            "Incorrect Username and Passowrd",
+                            "Please enter correct username and Password"));
+            return "login";
+        }
+    }
+
+    //logout event, invalidate session
+    public String logout() {
+        HttpSession session = SessionUtils.getSession();
+        session.invalidate();
+        return "login";
+    }
+
+    //This will be used at a later date, along with the md5 hashing
+    private static String byteArrToString(byte[] b) {
+        String res = null;
+        StringBuffer sb = new StringBuffer(b.length * 2);
+        for (int i = 0; i < b.length; i++) {
+            int j = b[i] & 0xff;
+            if (j < 16) {
+                sb.append('0');
+            }
+            sb.append(Integer.toHexString(j));
+        }
+        res = sb.toString();
+        return res.toUpperCase();
+    }
+
+    /* Commented out for now. This will be implemented for registration
+    Sets passwords to md5 hash (in theory)
+    
+     String passwordMD5 = null;
+     MessageDigest md5;
+     try {
+     md5 = MessageDigest.getInstance("MD5");
+     byte[] tmp = pwd.getBytes();
+     md5.update(tmp);
+     passwordMD5 = byteArrToString(md5.digest());
+     this.pwd = passwordMD5;
+     } catch (NoSuchAlgorithmException ex) {
+     Logger.getLogger(Login.class.getName()).log(Level.SEVERE, null, ex);
+     }              
+     */
+}
